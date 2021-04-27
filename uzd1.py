0d242495f7a4b2a0885d7f4d35fa763a5fd4715e
jansoneve <jansoneve@gmail.com>
2021-04-27 10:48:13 +0300

    Pirmais uzdevums

--- uzd1.py
+++ uzd1.py
@@ -6,9 +6,9 @@ Pārbaudiet programmas darbību ar dažādiem ievaddatiem.
 """
 import math
 
-radiuss=float(input("Ievadi riņķa līnijas rādiusu cm: "))
+radiuss=float(input("Ievadi riņķa līnijas rādiusu: "))
 
 garums=2*math.pi*radiuss
 laukums=math.pi*pow(radiuss,2)
 
-print("Riņķa līnijas garums ir ",garums,"cm. Riņķa līnijas laukums ir ",laukums,"cm.")
+print("Riņķa līnijas garums ir ",garums,". Riņķa līnijas laukums ir ",laukums,".")
